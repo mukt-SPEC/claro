1cc5e1af573ebe32c6c5096cdf6714b27e32feb0
mukt-SPEC <alaminsad6@gmail.com>
2025-06-26 23:16:57 +0100

    added hive

--- lib/model/todo_model.dart
+++ lib/model/todo_model.dart
@@ -1,3 +1,29 @@
-class Tasks{
+import 'package:hive/hive.dart';
+
+part 'todo_model.g.dart';
+@HiveType(typeId: 00)
+class Tasks extends HiveObject {
+  @HiveField(0)
+  String title;
+
+  @HiveField(1)
+  String description;
+
+  @HiveField(2)
+  bool isCompleted;
+  @HiveField(3)
+  DateTime? creationDate;
+
+  Tasks({
+    required this.title,
+    required this.description,
+    this.isCompleted = false,
+    required this.creationDate,
+  });
+
+  void toggleCompletion() {
+    isCompleted = !isCompleted;
+    save();
+  }
 
 }
---
+++ lib/model/todo_model.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'todo_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class TasksAdapter extends TypeAdapter<Tasks> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Tasks read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Tasks(
+      title: fields[0] as String,
+      description: fields[1] as String,
+      isCompleted: fields[2] as bool,
+      creationDate: fields[3] as DateTime?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Tasks obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.title)
+      ..writeByte(1)
+      ..write(obj.description)
+      ..writeByte(2)
+      ..write(obj.isCompleted)
+      ..writeByte(3)
+      ..write(obj.creationDate);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TasksAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
